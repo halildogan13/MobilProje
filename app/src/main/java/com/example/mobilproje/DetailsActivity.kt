2b507a0387c5b5aded7729a05f9d60330521e250
Kaan Berk Altay <kaanberkaltay@gmail.com>
2022-12-12 01:19:32 +0300

    activityler arası resim aktarımı yapıldı

--- app/src/main/java/com/example/mobilproje/DetailsActivity.kt
+++ app/src/main/java/com/example/mobilproje/DetailsActivity.kt
@@ -4,18 +4,33 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 
 class DetailsActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
-        var ss:String = intent.getStringExtra("game").toString()
+        var gameName:String = intent.getStringExtra("gameName").toString()
+        var gameImage: Int = intent.getIntExtra("gameImage",0)
+        var string:TextView = findViewById(R.id.textView5)
 
-        var string:TextView = findViewById(R.id.textView4)
+        var image:ImageView = findViewById(R.id.imageView2)
 
-        string.text = ss
+        image.setImageResource(gameImage)
+
+        string.text = gameName
+
+
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+
+
+
 
 
 
